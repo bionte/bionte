375f1d5b5a0f18331d562036180e77f1e21b9eb6
mdiaz <mdiaz@bionte.com>
2013-04-21 15:04:34 -0500

    Added Shell and code for bootstrapper. Now runs

--- Apps/YAPOS/Bootstrapper.cs
+++ Apps/YAPOS/Bootstrapper.cs
@@ -1,12 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Windows;
+
+using Microsoft.Practices.Prism.Modularity;
+using Microsoft.Practices.Unity;
+using Microsoft.Practices.Prism.UnityExtensions;
 
 namespace YAPOS
 {
-    class Bootstrapper
+    class Bootstrapper : UnityBootstrapper
     {
+        protected override DependencyObject CreateShell()
+        {
+            return this.Container.Resolve<Shell>();
+        }
+
+        protected override void InitializeShell()
+        {
+            base.InitializeShell();
+
+            App.Current.MainWindow = (Window)this.Shell;
+            App.Current.MainWindow.Show();
+        }
+
+
+        protected override IModuleCatalog CreateModuleCatalog()
+        {
+            return new ConfigurationModuleCatalog();
+        }
     }
 }
