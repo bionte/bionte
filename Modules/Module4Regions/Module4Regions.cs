7ff63d11785c70ad6537f05f43a4a19214cd711f
mdiaz <mdiaz@bionte.com>
2013-05-14 09:30:03 -0500

    updates for github

---
+++ Modules/Module4Regions/Module4Regions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Practices.Prism.Modularity;
+using Microsoft.Practices.Prism.Regions;
+
+using Bionte.Views;
+
+namespace Bionte.Modules
+{
+    class Module4Regions : IModule
+    {
+        private readonly IRegionViewRegistry regionViewRegistry;
+
+        public Module4Regions(IRegionViewRegistry registry)
+        {
+            this.regionViewRegistry = registry;   
+        }
+
+        public void Initialize()
+        {
+            regionViewRegistry.RegisterViewWithRegion("MainRegion", typeof(View4Regions));
+        }
+    }
+}
